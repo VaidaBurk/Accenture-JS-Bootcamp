0c23da557be4e1b53a0b3c623690b95dd50ee6b7
VaidaBurk <v.burkauskaite@gmail.com>
2022-07-22 21:37:41 +0300

    bootstrap added

--- 07_22_practical/addCustomer.php
+++ 07_22_practical/addCustomer.php
@@ -28,12 +28,19 @@ if ($_POST["FirstName"] === "" || $_POST["LastName"] === "" || $_POST["Email"] =
 
 
 <html>
-    <head></head>
+<head>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>    
+    </head>
     <body>
-        <?php if ($errorMessage != "") { ?>
-            <h1 style="color:red"><?= $errorMessage ?></h1>
-        <?php } else { ?>
-            <h1 style="color:green"><?= $success?></h1> 
-        <?php } ?>
+        <div class="container p-3">
+            <?php if ($errorMessage != "") { ?>
+                <div class="alert alert-danger" role="alert"><?= $errorMessage ?></div>
+            <?php } else { ?>
+                <div class="alert alert-success" role="alert"><?= $success?></div> 
+            <?php } ?>
+        </div>
     </body>
 </html>
