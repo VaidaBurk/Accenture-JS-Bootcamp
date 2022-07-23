b33ca680b1f3b6b15d02fae773530867f6325aab
VaidaBurk <v.burkauskaite@gmail.com>
2022-07-23 13:29:31 +0300

    07-23 practical task

--- 07_22_practical/addCustomer.php
+++ 07_22_practical/addCustomer.php
@@ -15,8 +15,8 @@ if ($_POST["FirstName"] === "" || $_POST["LastName"] === "" || $_POST["Email"] =
     $errorMessage = $connection->connect_error;
     }
     else {
-        $querry = "INSERT INTO customers (firstname, lastname, email, phone)
-        VALUES ('" . $_POST["FirstName"] . "','" . $_POST["LastName"] . "', '" . $_POST["Email"] . "','" . $_POST["Phone"] . "')";
+        $querry = "INSERT INTO customers (firstname, lastname, email, phone, comments, photo)
+        VALUES ('" . $_POST["FirstName"] . "','" . $_POST["LastName"] . "', '" . $_POST["Email"] . "','" . $_POST["Phone"] . "','" . $_POST["Comments"] . "','" . $_POST["Photo"] . "')";
         if ($connection->query($querry)) {
             $success = "Customer succesfully added."; 
         } else {
@@ -28,19 +28,20 @@ if ($_POST["FirstName"] === "" || $_POST["LastName"] === "" || $_POST["Email"] =
 
 
 <html>
-<head>
-        <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <!-- JavaScript Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>    
-    </head>
-    <body>
-        <div class="container p-3">
-            <?php if ($errorMessage != "") { ?>
-                <div class="alert alert-danger" role="alert"><?= $errorMessage ?></div>
-            <?php } else { ?>
-                <div class="alert alert-success" role="alert"><?= $success?></div> 
-            <?php } ?>
-        </div>
-    </body>
+
+<?php
+    include("header.php");
+?>
+
+<body>
+    <?php include("navbar.php"); ?>
+    <div class="container p-3">
+        <?php if ($errorMessage != "") { ?>
+        <div class="alert alert-danger" role="alert"><?= $errorMessage ?></div>
+        <?php } else { ?>
+        <div class="alert alert-success" role="alert"><?= $success?></div>
+        <?php } ?>
+    </div>
+</body>
+
 </html>
