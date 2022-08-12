16b787a2fcc32435842d95901e40ab73db952485
VaidaBurk <v.burkauskaite@gmail.com>
2022-08-12 22:43:18 +0300

    08-12 fetching data from DB

--- 08_12_Files/AllFiles.php
+++ 08_12_Files/AllFiles.php
@@ -2,15 +2,31 @@
 include "CsvFile.php";
 include "JsonFile.php";
 include "Band.php";
+include "..\common\dbConnection.php";
 
 abstract class AllFiles
 {
     abstract public function readDataFromFile();
     abstract public function saveDataToFile(string $fileName, array $data);
 
-    public function fetchDataFromDB()
+    public static function fetchDataFromDB()
     {
-
+        $bandsArr = [];
+        $connection = connectToDB("music_bands");
+        $query = "SELECT * FROM bands";
+        $result = $connection->query($query);
+        while ($resultRow = $result->fetch_assoc()){
+            $band = new Band (
+                $title = $resultRow["Title"],
+                $leadArtist = $resultRow["Lead_artist"],
+                $genres = $resultRow["Genres"],
+                $yearFoundation = $resultRow["Year_of_foundation"],
+                $origin = $resultRow["Origin"],
+                $website = $resultRow["Website"],
+            );
+            array_push($bandsArr, $band);
+        }
+        return $bandsArr;
     }
 
     public function saveDataToDB()
--- 08_12_Files/navbar.php
+++ 08_12_Files/navbar.php
@@ -12,7 +12,7 @@
           <a class="nav-link active" aria-current="page" href="displayFileReader.php">FILE READER</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">BANDS IN DB</a>
+          <a class="nav-link" href="displayAllBands.php">BANDS IN DB</a>
         </li>
       </ul>
     </div>
